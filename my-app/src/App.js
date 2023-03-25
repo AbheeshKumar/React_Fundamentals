a66d773ac1aca2a03f06d0c877df465ac2bb5256
Abheesh <abhesh.khemani@gmail.com>
2023-03-26 02:21:12 +0500

    First Components

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {Profile} from './Gallery'
+import Gallery from './Gallery.js';
+
+function App() {
+  return (
+    <section>
+    <Gallery/>
+    <h1>Winner</h1>
+    <Profile/>
+    </section>
+  );
+}
+
+export default App;
